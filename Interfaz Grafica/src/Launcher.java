3ca88c15d7ba49f57119bce920cccf2686879e14
DavidBohorquez <blueplayer-10@hotmail.com>
2019-05-25 23:56:08 -0500

    Add Launcher class

---
+++ Interfaz Grafica/src/Launcher.java
@@ -0,0 +1,18 @@
+
+import presentacion.vistas.VistaPrincipal;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author David Bohorquez
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        VistaPrincipal v = new VistaPrincipal();
+    }
+}
